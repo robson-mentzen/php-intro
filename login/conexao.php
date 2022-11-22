9a6e847b3ff24d82cad35e733a006d0dd080c8a4
ROBSON KUNZLER MENTZEN <robson.mentzen@acad.univates>
2022-11-22 19:10:23 -0300

    comunicação com banco de dados

--- login/conexao.php
+++ login/conexao.php
@@ -3,3 +3,5 @@
 $conexao = require('./banco.php');
 
 echo $conexao;
+
+//pegar os dados que vieram no formulário e testar se são válidos
--- login/config.php
+++ login/config.php
@@ -8,14 +8,11 @@ const DBUSER = 'root';
 const DBPASS = '';
 const DBPORT = '3306';
 
-try
-{
-$con = new PDO(DBDRIVE . ':host=' . DBHOST .
-';port='. DBPORT .';charset=utf8;dbname=' . DBNAME, DBUSER, DBPASS);
+try {
+    $con = new PDO(DBDRIVE . ':host=' . DBHOST .
+        ';port='. DBPORT .';charset=utf8;dbname=' . DBNAME, DBUSER, DBPASS);
 
-return $con;
-}
-catch (PDOException $e)
-{
-echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
+    return $con;
+} catch (PDOException $e) {
+    echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
 }
